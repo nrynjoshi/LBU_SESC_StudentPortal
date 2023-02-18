f3b8080611670144c1cb0078b1a7fe0247dc82cb
nrynjoshi <narayan@uslegalpro.com>
2023-02-18 09:40:46 +0000

    just few cleanup

--- studentportal/src/main/java/com/narayanjoshi/lbu/sesc/studentportal/controller/AppController.java
+++ studentportal/src/main/java/com/narayanjoshi/lbu/sesc/studentportal/controller/AppController.java
@@ -1,5 +1,7 @@
 package com.narayanjoshi.lbu.sesc.studentportal.controller;
 
+import com.narayanjoshi.lbu.sesc.studentportal.constant.Endpoint;
+import com.narayanjoshi.lbu.sesc.studentportal.dto.ResponseDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,21 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(value = Endpoint.ROOT_API_V1)
 public class AppController {
 
     @Value("${spring.application.name}")
     String appName;
 
-
-    public AppController(){
-        System.out.println("Init App Controller");
-    }
-
-    @GetMapping(value = "/app")
-    public @ResponseBody ResponseEntity<String> getAppInfo(){
-        return new ResponseEntity<>(appName, HttpStatus.OK);
+    @GetMapping(value = Endpoint.APP_URI)
+    public @ResponseBody ResponseEntity<ResponseDTO> getAppInfo(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("app_name", appName);
+        return new ResponseEntity<ResponseDTO>(new ResponseDTO(map), HttpStatus.OK);
     }
 
 }
