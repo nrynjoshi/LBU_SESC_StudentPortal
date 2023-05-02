92550782e07ddfda7f748dcd90226a7e0c031f59
nrynjoshi <narayan@uslegalpro.com>
2023-05-02 21:59:58 +0100

    work on message display success and error one

--- studentportal/src/main/java/com/narayanjoshi/lbu/sesc/studentportal/exception/UserAlreadyEnrollIntoCourseException.java
+++ studentportal/src/main/java/com/narayanjoshi/lbu/sesc/studentportal/exception/UserAlreadyEnrollIntoCourseException.java
@@ -3,7 +3,7 @@ package com.narayanjoshi.lbu.sesc.studentportal.exception;
 public class UserAlreadyEnrollIntoCourseException extends Exception{
 
 	public UserAlreadyEnrollIntoCourseException(String courseTitle){
-		super("User has already enroll into this course "+courseTitle);
+		super("User has already enrol into this course "+courseTitle);
 	}
 	
 }
